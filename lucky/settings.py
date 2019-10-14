6d7578eb4f2b8f8195ad7cb1807e6db0cb11e396
sarfraz19 <fakrudeen998@gmail.com>
2019-10-14 23:56:55 +0530

    new

--- lucky/settings.py
+++ lucky/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -117,6 +118,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
+STATICFILES_STORAGE = "whitenoise.django.GzipManifestStaticFilesStorage"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
